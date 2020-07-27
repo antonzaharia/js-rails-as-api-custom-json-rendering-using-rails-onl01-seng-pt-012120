bcf4c0b19f3717d37ad62d4d941023711579c63c
ANTON ZAHARIA <zaharia_anton@yahoo.com>
2020-07-27 18:56:48 +0000

    Automatically backed up by Learn

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -5,6 +5,6 @@ class BirdsController < ApplicationController
   end
   def show
     bird = Bird.find_by(id: params[:id])
-    render json: birds, only: [:id, :name, :species]
+    render json: bird, only: [:id, :name, :species]
   end
 end
